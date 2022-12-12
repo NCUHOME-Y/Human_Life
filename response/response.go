01335a50ecf9776dcef527823d1f550067b4356e
uifak <2576496262@qq.com>
2022-12-12 09:16:42 +0800

    finish and debug all mod

--- response/response.go
+++ response/response.go
@@ -42,7 +42,7 @@ func Login(ctx *gin.Context) {
 	var UserExist define.UserLogin
 	err := ctx.ShouldBind(&User)
 	if err != nil {
-		zapLog.SugarLogger.Info(err)
+		zapLog.SugarLogger.Info(err.Error())
 		ctx.JSON(500, gin.H{
 			"message": "用户名或密码格式有误",
 			"error":   err,
@@ -52,7 +52,7 @@ func Login(ctx *gin.Context) {
 
 	err = tx.Model(&define.UserLogin{}).Where("user_name=?", User.UserName).Find(&UserExist).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug(err)
+		zapLog.SugarLogger.Debug(err.Error())
 		ctx.JSON(400, gin.H{
 			"error": err,
 		})
@@ -69,7 +69,7 @@ func Login(ctx *gin.Context) {
 	if UserExist.UserName == User.UserName && UserExist.PassWord == User.PassWord {
 		Token, err := GetJwt(UserExist.UserName)
 		if err != nil {
-			zapLog.SugarLogger.Debug(err)
+			zapLog.Logger.Debug(err.Error())
 			ctx.JSON(500, gin.H{
 				"message": err,
 			})
@@ -81,7 +81,7 @@ func Login(ctx *gin.Context) {
 		}
 		err = tx.Model(&define.Token{}).Create(&Newtoken).Error
 		if err != nil {
-			zapLog.SugarLogger.Debug(err)
+			zapLog.SugarLogger.Debug(err.Error())
 			ctx.JSON(500, gin.H{
 				"message": "create token error",
 			})
@@ -106,32 +106,24 @@ func Login(ctx *gin.Context) {
 
 func AuthLogined() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var UserLogined define.UserLogin
 		var usertoken define.Token
-		err := ctx.ShouldBind(&UserLogined)
 		Token := ctx.Request.Header.Get("token")
+		err := initDatabase.DB.Model(&define.Token{}).Where("token=?", Token).Find(&usertoken).Error
 		if err != nil {
-			zapLog.SugarLogger.Info("bind error", err)
-			ctx.JSON(400, gin.H{
-				"message": "输入的格式有误",
-				"error":   err,
-			})
-			return
-		}
-		err = initDatabase.DB.Model(&define.Token{}).Where("uid=?", UserLogined.UserName).Find(&usertoken).Error
-		if err != nil {
-			zapLog.SugarLogger.Debug("Find error", err)
+			zapLog.SugarLogger.Debug("Find error", err.Error())
 			ctx.JSON(500, gin.H{
 				"message": "server error",
 				"error":   err,
 			})
 			return
 		}
-		if usertoken.Token != Token {
+		if define.CheckAmt(usertoken) {
 			zapLog.SugarLogger.Info("token 不正确")
 			ctx.JSON(400, gin.H{
 				"Message": "token error 无权访问",
 			})
+			ctx.Abort()
+			return
 		}
 		ctx.Next()
 	}
@@ -143,7 +135,7 @@ func Register(ctx *gin.Context) {
 	var User define.UserLogin
 	err := ctx.ShouldBind(&NewUser)
 	if err != nil {
-		zapLog.SugarLogger.Info(err)
+		zapLog.SugarLogger.Info(err.Error())
 		ctx.JSON(400, gin.H{
 			"message": "bind register err please check your post json",
 			"error":   err,
@@ -152,7 +144,7 @@ func Register(ctx *gin.Context) {
 	}
 	err = tx.Model(&define.UserLogin{}).Where("user_name=?", NewUser.UserName).Find(&User).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug("find error", err)
+		zapLog.SugarLogger.Debug("find error", err.Error())
 		ctx.JSON(500, err)
 		return
 	}
@@ -164,7 +156,7 @@ func Register(ctx *gin.Context) {
 	} else {
 		err = tx.Model(&define.UserLogin{}).Create(&NewUser).Error
 		if err != nil {
-			zapLog.SugarLogger.Debug(err)
+			zapLog.SugarLogger.Debug(err.Error())
 			ctx.JSON(500, gin.H{
 				"message": "create error",
 			})
@@ -219,7 +211,7 @@ func PostTimeRemind(ctx *gin.Context) {
 	}
 	err = tx.Model(&define.TimeRemind{}).Create(&bulletinBoard).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug(err)
+		zapLog.SugarLogger.Debug(err.Error())
 		ctx.JSON(500, gin.H{
 			"message": "create error",
 		})
@@ -234,39 +226,40 @@ func PostTimeRemind(ctx *gin.Context) {
 }
 
 func GetTimeRemind(ctx *gin.Context) {
-	var GetTimeRemind define.TimeRemind
-	var ReadDB define.TimeRemind
-	err := ctx.ShouldBind(&GetTimeRemind)
+	var GetTimeRemind struct {
+		RoomID string `json:"roomID"`
+	}
+	var ReadDB []define.TimeRemind
+	err := ctx.BindJSON(&GetTimeRemind)
 	if err != nil {
-		zapLog.SugarLogger.Info(err)
+		zapLog.SugarLogger.Info(err.Error())
 		ctx.JSON(400, gin.H{
 			"message": "bind register err please check your post json",
 			"error":   err,
 		})
 		return
 	}
-	err = initDatabase.DB.Model(&define.TimeRemind{}).Where("time=?,room_id=?)", GetTimeRemind.Time, GetTimeRemind.RoomId).Find(&ReadDB).Error
+	err = initDatabase.DB.Model(&define.TimeRemind{}).Where("room_id=?", GetTimeRemind.RoomID).Find(&ReadDB).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug(err)
+		zapLog.SugarLogger.Debug(err.Error())
 		ctx.JSON(500, gin.H{
 			"message": "find error",
 		})
 		return
 	}
-	ctx.JSON(200, gin.H{
-		"time":    ReadDB.Time,
-		"message": ReadDB.Message,
-	})
+	ctx.JSON(200, ReadDB)
 }
 
 // PostDuty OnDuty 值日生功能
 func PostDuty(ctx *gin.Context) {
 	tx := initDatabase.DB.Begin()
+	var NeedDuty define.NeedDuty
 	var Order define.GetDuty
 	err := ctx.ShouldBind(&Order)
+	NeedDuty = Order.NeedDuty
 	people := []string{Order.P1, Order.P2, Order.P3, Order.P4}
 	if err != nil {
-		zapLog.SugarLogger.Info(err)
+		zapLog.SugarLogger.Info(err.Error())
 		ctx.JSON(400, gin.H{
 			"message": "bind register err please check your post json",
 			"error":   err,
@@ -280,22 +273,53 @@ func PostDuty(ctx *gin.Context) {
 		}
 		err := tx.Model(&define.DutyOrder{}).Create(&Duty).Error
 		if err != nil {
-			zapLog.SugarLogger.Debug("create error", err)
+			zapLog.SugarLogger.Debug("create error", err.Error())
 			tx.Rollback()
 			return
 		}
 	}
+	err = tx.Model(&define.NeedDuty{}).Create(&NeedDuty).Error
+	if err != nil {
+		zapLog.SugarLogger.Debug("create error", err.Error())
+		tx.Rollback()
+		ctx.JSON(500, err.Error())
+		return
+	}
 	tx.Commit()
 	ctx.JSON(200, gin.H{
-		"message": "query Ok",
+		"message":  "query Ok",
+		"NeedDuty": NeedDuty,
 	})
 }
 
-//func GetDuty(ctx *gin.Context) {
-//     var Get struct{
-//		 RoomID  string `json:"roomID"`
-//		 Today   uint  `json:"today"`
-//	 }
+var i int
+
+func GetDuty(ctx *gin.Context) {
+	var Get struct {
+		RoomID string
+	}
+	var Result []define.DutyOrder
+	err := ctx.BindJSON(&Get)
+	if err != nil {
+		zapLog.SugarLogger.Info(err.Error())
+		ctx.JSON(400, gin.H{
+			"message": "check json",
+		})
+		return
+	}
+	err = initDatabase.DB.Model(&define.DutyOrder{}).Where("room_id=?", Get.RoomID).Find(&Result).Error
+	if err != nil {
+		zapLog.SugarLogger.Debug(err.Error())
+		ctx.JSON(500, gin.H{
+			"message": "find error",
+			"error":   err,
+		})
+		return
+	}
+	ctx.JSON(200, Result[i])
+	i++
+}
+
 //
 //}
 
@@ -306,8 +330,9 @@ func AddPartner(ctx *gin.Context) {
 	var Partner define.UserLogin
 	var ExistUser define.UserLogin
 	err := ctx.ShouldBind(&NewPartner)
+	fmt.Println(err, NewPartner)
 	if err != nil {
-		zapLog.SugarLogger.Info(err)
+		zapLog.SugarLogger.Info(err.Error())
 		ctx.JSON(400, gin.H{
 			"message": "bind register err please check your post json",
 			"error":   err,
@@ -316,7 +341,7 @@ func AddPartner(ctx *gin.Context) {
 	}
 	err = tx.Model(&define.UserLogin{}).Where("user_name=?", NewPartner.UserName).Find(&Partner).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug("find error", err)
+		zapLog.SugarLogger.Debug("find error", err.Error())
 		ctx.JSON(500, err)
 		return
 	}
@@ -329,7 +354,7 @@ func AddPartner(ctx *gin.Context) {
 	}
 	err = tx.Model(&define.UserLogin{}).Where("user_name=?", NewPartner.UserName).Find(&ExistUser).Error
 	if err != nil {
-		zapLog.SugarLogger.Debug("find error", err)
+		zapLog.SugarLogger.Debug("find error", err.Error())
 		ctx.JSON(500, gin.H{
 			"message": "server error",
 			"error":   err,
@@ -339,23 +364,24 @@ func AddPartner(ctx *gin.Context) {
 	var ID string
 	if ExistUser.RoomID == "" {
 		ID = GetRoomID(6)
-		err := tx.Model(&ExistUser).Select("room_id").Updates(define.UserLogin{RoomID: ID}).Error
+		err := tx.Model(&define.UserLogin{}).Where("user_name=?", ExistUser.UserName).Select("room_id").Updates(define.UserLogin{RoomID: ID}).Error
 		if err != nil {
-			zapLog.SugarLogger.Debug("Update error")
+			zapLog.SugarLogger.Debug("Update error", err.Error())
 			ctx.JSON(500, err)
 			tx.Rollback()
 			return
 		}
 
-		err = tx.Model(&Partner).Select("room_id").Updates(define.UserLogin{RoomID: ID}).Error
+		err = tx.Model(&define.UserLogin{}).Where("user_name=?", NewPartner.PartnerNumber).Select("room_id").Updates(define.UserLogin{RoomID: ExistUser.RoomID}).Error
 		if err != nil {
-			zapLog.SugarLogger.Debug("Update error")
+			zapLog.SugarLogger.Debug("Update error", err.Error())
 			ctx.JSON(500, err)
 			tx.Rollback()
 			return
 		}
 	} else {
-		err = tx.Model(&Partner).Select("room_id").Updates(define.UserLogin{RoomID: ExistUser.RoomID}).Error
+		ID = ExistUser.RoomID
+		err = tx.Model(&define.UserLogin{}).Where("user_name=?", NewPartner.PartnerNumber).Select("room_id").Updates(define.UserLogin{RoomID: ExistUser.RoomID}).Error
 		if err != nil {
 			zapLog.SugarLogger.Debug("Update error")
 			ctx.JSON(500, err)
@@ -364,6 +390,7 @@ func AddPartner(ctx *gin.Context) {
 		}
 	}
 	tx.Commit()
+	fmt.Println(ExistUser)
 	ctx.JSON(200, gin.H{
 		"message": "添加成功",
 		"roomId":  ID,
@@ -384,10 +411,13 @@ func GetRoomID(width int) string {
 	return sb.String()
 }
 func GetPartnerMessage(ctx *gin.Context) {
-	var User define.UserLogin
+	var User struct {
+		UserName uint `json:"UserName"`
+	}
 	var UserExist define.UserLogin
 	var Users []define.UserLogin
-	err := ctx.ShouldBind(&User)
+	err := ctx.BindJSON(&User)
+	fmt.Println(User)
 	if err != nil {
 		zapLog.SugarLogger.Info(err)
 		ctx.JSON(400, gin.H{
@@ -396,13 +426,14 @@ func GetPartnerMessage(ctx *gin.Context) {
 		})
 		return
 	}
-	err = initDatabase.DB.Model(&User).Find(&UserExist).Error
+	err = initDatabase.DB.Model(&define.UserLogin{}).Where("user_name=?", User.UserName).Find(&UserExist).Error
 	if err != nil {
 		zapLog.SugarLogger.Debug("find error", err)
 		ctx.JSON(500, err)
 		return
 
 	}
+	fmt.Println(UserExist)
 	err = initDatabase.DB.Model(&define.UserLogin{}).Where("room_id=?", UserExist.RoomID).Find(&Users).Error
 	if err != nil {
 
@@ -421,7 +452,7 @@ func ChangeStates(ctx *gin.Context) {
 		UserName uint   `json:"UserName"`
 		State    string `json:"State"`
 	}
-	err := ctx.ShouldBind(&State)
+	err := ctx.BindJSON(&State)
 	if err != nil {
 		zapLog.SugarLogger.Info(err)
 		ctx.JSON(400, gin.H{
@@ -445,10 +476,10 @@ func ChangeStates(ctx *gin.Context) {
 
 func GetStateMessage(ctx *gin.Context) {
 	var User struct {
-		UserName uint
+		UserName uint `json:"UserName"`
 	}
 	var Message define.UserLogin
-	err := ctx.ShouldBind(&User)
+	err := ctx.BindJSON(&User)
 	if err != nil {
 		zapLog.SugarLogger.Info(err)
 		ctx.JSON(400, gin.H{
@@ -466,4 +497,76 @@ func GetStateMessage(ctx *gin.Context) {
 		})
 	}
 	ctx.JSON(200, Message.State)
+}
+
+//公告板
+
+func PostBoard(ctx *gin.Context) {
+	tx := initDatabase.DB.Begin()
+	var NewMessage define.Board
+	var Message define.Board
+	err := ctx.ShouldBind(&NewMessage)
+	if err != nil {
+		zapLog.SugarLogger.Info(err)
+		ctx.JSON(400, gin.H{
+			"Message": "check post json",
+		})
+	}
+	err = initDatabase.DB.Model(&define.Board{}).Where("room_id=?", NewMessage.RoomID).Find(&Message).Error
+	if err != nil {
+		zapLog.SugarLogger.Debug(err.Error())
+		ctx.JSON(500, err)
+		return
+	}
+	if !define.CheckAmt(Message) {
+		err = tx.Model(&define.Board{}).Where("room_id=?", NewMessage.RoomID).Delete(&Message).Error
+		if err != nil {
+			zapLog.SugarLogger.Debug(err.Error())
+			ctx.JSON(500, err)
+			tx.Rollback()
+			return
+		}
+		err = tx.Model(&define.Board{}).Create(&NewMessage).Error
+		if err != nil {
+			zapLog.SugarLogger.Debug(err.Error())
+			ctx.JSON(500, err)
+			tx.Rollback()
+			return
+		}
+	} else {
+		err = tx.Model(&define.Board{}).Create(&NewMessage).Error
+		if err != nil {
+			zapLog.SugarLogger.Debug(err.Error())
+			ctx.JSON(500, err)
+			tx.Rollback()
+			return
+		}
+	}
+	tx.Commit()
+	ctx.JSON(200, gin.H{
+		"message": "query ok",
+		"公告":      NewMessage.Message,
+	})
+}
+
+func GetBoard(ctx *gin.Context) {
+	var Get struct {
+		RoomID string `json:"RoomID"`
+	}
+	var Message define.Board
+	err := ctx.BindJSON(&Get)
+	if err != nil {
+		zapLog.SugarLogger.Info(err.Error())
+		ctx.JSON(400, gin.H{
+			"message": "check your json",
+			"error":   err,
+		})
+	}
+	err = initDatabase.DB.Model(&define.Board{}).Where("room_id=?", Get.RoomID).Find(&Message).Error
+	if err != nil {
+		zapLog.SugarLogger.Debug(err.Error())
+		ctx.JSON(500, err)
+		return
+	}
+	ctx.JSON(200, Message)
 }
